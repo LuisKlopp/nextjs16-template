b41820bc88c1143b2f11b4c633bc97b94e83a1b9
LuisKlopp <fbgus333@gmail.com>
2026-02-15 01:47:41 +0900

    Style: 마지막 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <span className="text-4xl">nextjs 16 template</span>
+      <span className="text-4xl text-red-800">nextjs 16 template</span>
     </div>
   );
 }
