e95cb249fd3959c2c48a980fc50c58e2e75703c2
LuisKlopp <fbgus333@gmail.com>
2026-02-15 01:47:08 +0900

    Change: 타이틀 텍스트 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="h-full w-full">
-      <span className="text-1xl">fdsdfsafdasfsdads</span>
+    <div className="flex h-full w-full items-center justify-center">
+      <span className="text-4xl">nextjs 16 template</span>
     </div>
   );
 }
